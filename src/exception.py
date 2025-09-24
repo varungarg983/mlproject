9685d50a8685794ca7b327433786f8d92a97541f
varungarg983 <varungarg983@gmail.com>
2025-09-25 10:01:39 +1200

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    return f"Error occurred in script: {file_name} at line number: {line_number} with message: {str(error)}"
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        super().__init__(error)
+        self.error = error
+        self.error_detail = error_detail
+
+    def __str__(self):
+        return error_message_detail(self.error, self.error_detail)
---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
+
+
+LOG_FILE_PATH = os.path.join(LOG_PATH, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+)
+
